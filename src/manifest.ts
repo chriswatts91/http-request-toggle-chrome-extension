ce5a0dab949d4c7413ecf469ebf07d1e663f2e00
Christopher Watts <chris@cawserve.com>
2023-03-19 17:17:14 +0000

    feat: Bump version

--- src/manifest.ts
+++ src/manifest.ts
@@ -3,7 +3,7 @@ import { defineManifest } from "@crxjs/vite-plugin";
 export default defineManifest({
   name: "HTTP Request Toggle",
   description: "",
-  version: "0.1.0",
+  version: "0.1.1",
   manifest_version: 3,
   icons: {
     "16": "img/play-16.png",
